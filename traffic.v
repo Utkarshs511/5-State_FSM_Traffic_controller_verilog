089f422f39bff79a16ee7823b6470adf9f41a377
Utkarshs511 <utkarsh.20235091@mnnit.ac.in>
2025-06-15 17:51:10 +0530

    Update traffic.v

--- traffic.v
+++ traffic.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.10.2024 21:59:43
-// Design Name: 
-// Module Name: traffic
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module traffic(
     input clk,                 // Clock signal
